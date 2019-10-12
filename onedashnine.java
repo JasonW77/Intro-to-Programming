b8db9d5e19083b296763243c0974de4f7182c7de
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-11 21:37:40 -0600

    Update onedashnine.java

--- onedashnine.java
+++ onedashnine.java
@@ -6,8 +6,13 @@
 public class onedashthirteen {
    
     public static void main(String[] args) {
-      System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; 
-      System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ;
+       
+      System.out.println("Author Jason Waters");
+      System.out.println("Date 10/11/2019");
+      System.out.println("Assignment 1-5");
+       
+      System.out.println("X"); System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; 
+      System.out.println("Y"); System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ;
     }
       
 }                  
