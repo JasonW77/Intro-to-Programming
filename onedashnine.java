f516ad50d1f56f3127215fb390caf7b6f2377338
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-12 02:01:19 -0600

    added comments for area and perimeter

--- onedashnine.java
+++ onedashnine.java
@@ -6,7 +6,10 @@ public class onedashnine {
    
     public static void main(String[] args) {
        
+      //Calculate Area
       System.out.println ("Area") ; System.out.println (4.5 * 7.9) ; 
+       
+      //Calculate perimeter
       System.out.println ("Perimeter") ; System.out.println ((4.5 * 2) + (7.9 * 2)) ;
     
     }}                  
