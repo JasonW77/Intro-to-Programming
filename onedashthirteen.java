15dac0e2fabade75e17d0d20ac2a22bd531539d4
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-12 02:00:00 -0600

    moved comments

--- onedashthirteen.java
+++ onedashthirteen.java
@@ -6,7 +6,10 @@ public class onedashthirteen {
    
     public static void main(String[] args) {
        
-      System.out.println("X"); System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; // solve for X
-      System.out.println("Y"); System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; // solve for Y
+      // solve for X
+      System.out.println("X"); System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ;
+       
+      // solve for Y
+      System.out.println("Y"); System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; 
     
     }}
