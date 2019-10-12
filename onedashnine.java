57d6a68aa2b305a3819fc3866bfff5f5a92465cc
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-11 21:48:43 -0600

    corrections and adjustments. see extended.
    added Area, and Perimeter to printout to distinguish between numbers.
    changed code from onedashthirteen to onedashnine, 
    changed public class to reflect onedashnine change.

--- onedashnine.java
+++ onedashnine.java
@@ -3,16 +3,16 @@
  * @author Jason Waters
  * 10/11/2019
  */
-public class onedashthirteen {
+public class onedashnine {
    
     public static void main(String[] args) {
        
       System.out.println("Author Jason Waters");
       System.out.println("Date 10/11/2019");
-      System.out.println("Assignment 1-5");
+      System.out.println("Assignment 1-9");
        
-      System.out.println("X"); System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; 
-      System.out.println("Y"); System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ;
+      System.out.println ("Area") ;System.out.println (4.5 * 7.9) ; 
+      System.out.println ("Perimeter") ;System.out.println ((4.5 * 2) + (7.9 * 2)) 
     }
       
 }                  
