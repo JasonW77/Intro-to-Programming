95e35a78794b7b5975b95210dde23c3de091149b
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-11 22:03:22 -0600

    fixed spacing and added a missing semicolon

--- onedashnine.java
+++ onedashnine.java
@@ -7,12 +7,12 @@ public class onedashnine {
    
     public static void main(String[] args) {
        
-      System.out.println("Author Jason Waters");
-      System.out.println("Date 10/11/2019");
-      System.out.println("Assignment 1-9");
+      System.out.println ("Author Jason Waters") ;
+      System.out.println ("Date 10/11/2019") ;
+      System.out.println ("Assignment 1-9") ;
        
-      System.out.println ("Area") ;System.out.println (4.5 * 7.9) ; 
-      System.out.println ("Perimeter") ;System.out.println ((4.5 * 2) + (7.9 * 2)) 
+      System.out.println ("Area") ; System.out.println (4.5 * 7.9) ; 
+      System.out.println ("Perimeter") ; System.out.println ((4.5 * 2) + (7.9 * 2)) ;
     }
       
 }                  
