90038ab031c850e674bd872fb9e695f28986f930
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-11 21:41:27 -0600

    moved info down in the printout.

--- onedash5.java
+++ onedash5.java
@@ -6,10 +6,13 @@
  */
 public class onedash5 {
     
-    System.out.println("Author Jason Waters");
-    System.out.println("Date 10/11/2019");
-    System.out.println("Assignment 1-5");
+
     public static void main(String[] args) {
+        
+      System.out.println("Author Jason Waters");
+      System.out.println("Date 10/11/2019");
+      System.out.println("Assignment 1-5");
+        
       System.out.println ((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
     }
   }                  
