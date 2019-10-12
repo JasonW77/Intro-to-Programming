0c497a9d27591f5a7be5b2504b5d537292f42747
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-12 01:47:37 -0600

    cleaned up lines see extended.
    changed prints to comments for Date, Name, and Assignment.
    removed excess lines.

--- onedash5.java
+++ onedash5.java
@@ -1,18 +1,10 @@
+// Author Jason Waters
+// Date 10/11/2019
+// Assignment 1-5
 
-/**
- *
- * @author Jason Waters
- * 10/11/2019
- */
 public class onedash5 {
     
-
     public static void main(String[] args) {
-        
-      System.out.println("Author Jason Waters");
-      System.out.println("Date 10/11/2019");
-      System.out.println("Assignment 1-5");
-        
-      System.out.println ((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
-    }
-  }                  
+       
+        System.out.println ((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }}
