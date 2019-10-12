8d578d9dc96307e4d97a4e4e593dd045156d7d8a
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-12 01:57:09 -0600

    changed prints to comments.
    changed prints to comments for Date, Name, and Assignment.
    added comments for solving for X and Y
    removed excess spaces

--- onedashthirteen.java
+++ onedashthirteen.java
@@ -1,17 +1,12 @@
-/**
- *
- * @author Jason Waters
- * 10/11/2019
- */
+// Author Jason Waters
+// Date 10/11/2019
+// Assignment 1-13
+
 public class onedashthirteen {
    
     public static void main(String[] args) {
-
-      System.out.println("Author Jason Waters");
-      System.out.println("Date 10/11/2019");
-      System.out.println("Assignment 1-13");
        
-      System.out.println("X"); System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; 
-      System.out.println("Y"); System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ;}
-      
-}                  
+      System.out.println("X"); System.out.println (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; // solve for X
+      System.out.println("Y"); System.out.println ((( 3.4 * 5.9 ) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1))) ; // solve for Y
+    
+    }}
