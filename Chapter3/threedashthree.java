6dbd19f0907fbb263f0292fd5dee7bd7f4ccd56f
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-17 01:51:47 -0600

    fixed spelling errors

--- Chapter3/threedashthree.java
+++ Chapter3/threedashthree.java
@@ -12,22 +12,22 @@ public class threedashthree {
 		
 		//Prompt the user to enter a, and assign to a.
 		System.out.print("Enter a: ") ;
-		//reads useers input for (a) in a integer value from the console
+		//reads users input for (a) in a integer value from the console
 		int a = input.nextInt() ;
 				
 		//Prompt the user to enter b, and assign to b.
 		System.out.print("Enter b: ") ;
-		//reads useers input for (b) in a integer value from the console
+		//reads users input for (b) in a integer value from the console
 		int b = input.nextInt() ;
 				
 		//Prompt the user to enter c, and assign to c.
 		System.out.print("Enter c: ") ;
-		//reads useers input for (c) in a integer value from the console
+		//reads users input for (c) in a integer value from the console
 		int c = input.nextInt() ;
 				
 		//Prompt the user to enter d, and assign to d.
 		System.out.print("Enter d: ") ;
-		//reads useers input for (d) in a integer value from the console
+		//reads users input for (d) in a integer value from the console
 		int d = input.nextInt() ;
 				
 		//Check to see if (ad - bc) = 0, if (ad - bc) = 0 stop, and display the message “The equation has no solution.”
@@ -38,12 +38,12 @@ public class threedashthree {
 			
 			//Prompt the user to enter e, and assign to e.
 			System.out.print("Enter e: ") ;
-			//reads useers input for (e) in a integer value from the console
+			//reads users input for (e) in a integer value from the console
 			int e = input.nextInt() ;
 		
 			//Prompt the user to enter f, and assign to f.
 			System.out.print("Enter f: ") ;
-			//reads useers input for (f) in a integer value from the console
+			//reads users input for (f) in a integer value from the console
 			int f = input.nextInt() ;
 		
 			//Solve for x, and y.
