844eccf185e4667cc8779096095264ba9eec04c5
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-18 09:08:22 -0600

    fixed capitalization errors in nextDouble

--- Chapter3/threedashthree.java
+++ Chapter3/threedashthree.java
@@ -13,22 +13,22 @@ public class threedashthree {
 		//Prompt the user to enter a, and assign to a.
 		System.out.print("Enter a: ") ;
 		//reads users input for (a) in a double value from the console
-		double a = input.nextdouble() ;
+		double a = input.nextDouble() ;
 				
 		//Prompt the user to enter b, and assign to b.
 		System.out.print("Enter b: ") ;
 		//reads users input for (b) in a double value from the console
-		double b = input.nextdouble() ;
+		double b = input.nextDouble() ;
 				
 		//Prompt the user to enter c, and assign to c.
 		System.out.print("Enter c: ") ;
 		//reads users input for (c) in a double value from the console
-		double c = input.nextdouble() ;
+		double c = input.nextDouble() ;
 				
 		//Prompt the user to enter d, and assign to d.
 		System.out.print("Enter d: ") ;
 		//reads users input for (d) in a double value from the console
-		double d = input.nextdouble() ;
+		double d = input.nextDouble() ;
 				
 		//Check to see if (ad - bc) = 0, if (ad - bc) = 0 stop, and display the message “The equation has no solution.”
 		if ( (a * d - b * c) == 0 ) {
@@ -39,12 +39,12 @@ public class threedashthree {
 			//Prompt the user to enter e, and assign to e.
 			System.out.print("Enter e: ") ;
 			//reads users input for (e) in a double value from the console
-			double e = input.nextdouble() ;
+			double e = input.nextDouble() ;
 		
 			//Prompt the user to enter f, and assign to f.
 			System.out.print("Enter f: ") ;
 			//reads users input for (f) in a double value from the console
-			double f = input.nextdouble() ;
+			double f = input.nextDouble() ;
 		
 			//Solve for x, and y.
 			double x = (e * d - b * f) / (a * d - b * c) ;
