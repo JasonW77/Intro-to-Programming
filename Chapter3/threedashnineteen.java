0ebe243577eadc0bb8254639178d680ca57dde9f
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-17 23:16:01 -0600

    changed int to double

--- Chapter3/threedashnineteen.java
+++ Chapter3/threedashnineteen.java
@@ -15,23 +15,23 @@ public class threedashnineteen {
 			//prompt the user to enter side1
 			System.out.print("Enter length of side 1: ") ;
 			//reads an input from the user in a double value from the console as side1
-			double side1 = input.nextInt() ;
+			double side1 = input.nextdouble() ;
 			
 			//prompt the user to enter side2
 			System.out.print("Enter length of side 2: ") ;
 			//reads an input from the user in a double value from the console as side2
-			double side2 = input.nextInt() ;
+			double side2 = input.nextdouble() ;
 			
 			//prompt the user to enter side3
 			System.out.print("Enter length of side 3: ") ;
 			//reads an input from the user in a double value from the console as side3
-			double side3 = input.nextInt() ;
+			double side3 = input.nextdouble() ;
 			
 			//check validity of inputs from user
 			if (side3 <= side1 + side2) { ;
 				System.out.println("input is not valid") ;
 			}
-			else {int perimeter = (side1+side2+side3) ;
+			else {double perimeter = (side1+side2+side3) ;
 				System.out.println("Perimeter is: " + perimeter) ;
 	}}
 }
