c8cb87fede105a8b8c3022c23718d87afd1ebabf
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-10-31 11:12:29 -0600

    added sixdashthirtyseven.java

---
+++ chapter6/sixdashthirtyseven.java
@@ -0,0 +1,45 @@
+// Author Jason Waters
+// Date 10/31/2019
+// Assignment 6-37
+
+import java.util.Scanner ;
+
+
+public class sixdashthirtyseven {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user to enter number and width
+		System.out.print("Enter number: ");
+		int number = input.nextInt();
+		System.out.print("Enter width: ");
+		int width = input.nextInt();
+		
+		//call format method
+		String ret = format(number, width);
+				
+		// Print final result	
+		System.out.print(ret);
+		
+	}
+	
+	public static String format(int number, int width){
+		
+		//convert number to string
+		String str = number + "";
+		String end = "";
+	
+		//creating 0's needed and putting them in end
+		for (int ret = width - str.length();ret > 0; ret--) {
+			end += "0"; 
+		}
+		
+		//combining 0's + number; 
+		end += str;
+		
+		//return value to main
+		return end;
+	}
+}
