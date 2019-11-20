320e28a83f5f6597d471981330842fd73dc4ecaf
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-11-20 09:24:36 -0700

    finished and corrected code

--- Chapter8/Assignment8_3.java
+++ Chapter8/Assignment8_3.java
@@ -1,41 +1,34 @@
 // Author Jason Waters
-// Date 11/19/2019
-// Chapter8 Assignment8_3
-public class Assignment8_3 {
+// Date 11/20/2019
+// Chapter8 Assignment8_37
+
+import java.util.Scanner ;
+
+public class Exercise8_37 {
 	public static void main(String[] args) {
 		
-		double[][] a = {
-			{1 , 2 , 3},
-			{4 , 5 , 6},
-			{7 , 8 , 9},
-		};
-		double[][] b = {
-			{0 , 2 , 4},
-			{1 , 4.5 , 2.2},
-			{1.1 , 4.3 , 5.2},
-		};
-		double[][] c = addMatrix(a, b);	
-			
-		for (int i = 0; i < a.length; i++){
-			for (int j = 0; j < b.length; j++){
-				System.out.print(c[i][j] + " ");
-			}
-			System.out.println(" ");
-		}	
-	}	
-	public static double[][] addMatrix(double[][] a, double[][] b){
-		double[][] c = {
-			{0 , 0 , 0},
-			{0 , 0 , 0},
-			{0 , 0 , 0},
-		};
 		
-		for (int i = 0; i < a.length; i++){
-			for (int j = 0; j < b.length; j++){
-				c[i][j] = (a[i][j] + b[i][j]);
-								
-			}
+		String[][] state = {
+			{"Alabama" , "Montgomery"},
+			{"Alaska" , "Juneau"},
+			{"Arizona" ,"Phoenix"},
+			{"Arkansas" , "Little Rock"},
+			{"California" , "Sacramento"},
+			{"Colorado" , "Denver"},
+			{"Connecticut" , "Hartford"},
+			{"Delaware" , "Dover"},
+			{"Florida" , "Tallahassee"},
+			{"Georgia" , "Atlanta"},
+		};
+		int cnt = 0;
+		Scanner input = new Scanner(System.in);
+		for (int i = 0; i < state.length; i++) {
+				System.out.println("What is the capital of " + state[i][0]);
+				String answer = input.nextLine();
+				if (answer.equalsIgnoreCase(state[i][1])) {
+					cnt++;
+				}
 		}
-		return c;
-	}
+		System.out.println("Total Correct Answers: " + cnt);
+	}	
 }
